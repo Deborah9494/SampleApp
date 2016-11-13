8d4a881e64e80726fa6baefe7f966956d750db63
Deborah9494 <deborah94.iori@gmail.com>
2016-11-13 14:33:29 +0100

    Add user microposts without images

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,7 @@ class UsersController < ApplicationController
 
   def show
   	@user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
 	end	
 
 	def new
@@ -53,7 +54,7 @@ class UsersController < ApplicationController
                                    :password_confirmation)
     end
     # Before filters
-
+=begin
     # Confirms a logged-in user.
     def logged_in_user
       unless logged_in?
@@ -62,6 +63,7 @@ class UsersController < ApplicationController
         redirect_to login_url
       end
     end
+=end
     # Confirms the correct user.
     def correct_user
       @user = User.find(params[:id])
